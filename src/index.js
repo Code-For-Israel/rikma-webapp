96847f82936d371732275104a527626a3fcf2cce
dantal4 <70883080+dantal4@users.noreply.github.com>
2023-07-27 08:34:44 +0300

    Use Hebrew as the only supported language (#3)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { createRoot } from 'react-dom/client';
 import { IntlProvider } from 'react-intl';
 
 import TinodeWeb from './views/tinode-web.jsx';
-import HashNavigation from './lib/navigation.js';
 
 // Insert google analytics script and tag if configured.
 if (typeof FIREBASE_INIT != 'undefined' && FIREBASE_INIT && FIREBASE_INIT.measurementId) {
@@ -22,38 +21,7 @@ if (typeof FIREBASE_INIT != 'undefined' && FIREBASE_INIT && FIREBASE_INIT.measur
   gtag('config', FIREBASE_INIT.measurementId);
 }
 
-// Allow loading translation strings for just one language.
-const messageLoader = {
-  'de': _ => import('./i18n.min/de.json'),
-  'en': _ => import('./i18n.min/en.json'),
-  'es': _ => import('./i18n.min/es.json'),
-  'fr': _ => import('./i18n.min/fr.json'),
-  'ko': _ => import('./i18n.min/ko.json'),
-  'ro': _ => import('./i18n.min/ro.json'),
-  'ru': _ => import('./i18n.min/ru.json'),
-  'zh': _ => import('./i18n.min/zh.json'),
-  'zh-TW': _ => import('./i18n.min/zh-TW.json')
-};
-
-// Detect human language to use in the UI:
-//  Check parameters from URL hash #?hl=ru, then browser, then use 'en' as a fallback.
-const { params } = HashNavigation.parseUrlHash(window.location.hash);
-const language = (params && params.hl) ||
-  (navigator.languages && navigator.languages[0]) ||
-  navigator.language ||
-  navigator.userLanguage ||
-  'en';
-
-// Make sure names like 'en_GB' and 'en-GB' consistently use '-'.
-const normalized = language.replace('_', '-');
-// Get the base language 'en' from a more specific 'en-US' as a partial fallback.
-const baseLanguage = normalized.split('-')[0].toLowerCase();
-
-// Try the full locale first, then the locale without the region code, fallback to 'en'.
-const htmlLang = messageLoader[normalized] ? language : messageLoader[baseLanguage] ? baseLanguage : 'en';
-
-// Set lang attribute of the HTML element: <html lang="XX">
-document.getElementsByTagName('html')[0].setAttribute('lang', htmlLang);
+const getHebrewTranslations = () => import('./i18n.min/he.json');
 
 // Clear caches in dev mode
 if (document.getElementsByTagName('html')[0].getAttribute('mode') === 'dev') {
@@ -67,9 +35,9 @@ if (document.getElementsByTagName('html')[0].getAttribute('mode') === 'dev') {
 
 // Render the app.
 const root = createRoot(document.getElementById('mountPoint'));
-messageLoader[htmlLang]().then(messages =>
+getHebrewTranslations().then(messages =>
   root.render(
-    <IntlProvider locale={language} messages={messages} textComponent={React.Fragment}>
+    <IntlProvider locale='he' messages={messages} textComponent={React.Fragment}>
       <TinodeWeb />
     </IntlProvider>
 ));
